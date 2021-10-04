3fd1ec7d56a2438ee13c5db4add24aafa92592ca
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-10-04 14:12:46 -0300

    Setup Project

---
+++ src/main/kotlin/br/com/erombi/estudomongodb/EstudomongodbApplication.kt
@@ -0,0 +1,11 @@
+package br.com.erombi.estudomongodb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstudomongodbApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstudomongodbApplication>(*args)
+}
